1177a971fb421335a2665105e03fb92b4c1cfad0
Paulo Raitz <paulo.jraitz@gmail.com>
2021-04-13 20:12:50 -0300

    Final project

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+namespace guessingGame
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int valorMinimo = 0;
+      int valorMaximo = 101;
+      const char maior = '>';
+      const char menor = '<';
+      const char igual = '=';
+      char validador = ' ';
+      int tentativa = 0;
+      int operador = 0;
+      int intera = 0;
+      var random = new Random();
+      System.Console.WriteLine($"Pense em um número de 1 à 100... Pensou?");
+      Thread.Sleep(3000);
+      System.Console.WriteLine($"Se for maior que {operador}, digite: '{maior}'\nSe for menor que {operador}, digite: '{menor}'\nSe for igual a {operador}, digite: '{igual}'\n  ");
+      do
+      {
+        operador = random.Next(valorMinimo, valorMaximo);
+        Console.WriteLine($" O número é: {operador}?");
+        validador = char.Parse(Console.ReadLine());
+        if (validador == igual)
+        {
+          System.Console.WriteLine($"I m the genius acertei seu número em {intera}");
+          break;
+        }
+
+        if (validador == maior)
+          valorMinimo = operador + 1;
+        else if (validador == menor)
+          valorMaximo = operador - 1;
+
+        intera = intera + 1;
+      } while (validador == igual || tentativa < 8);
+      tentativa++;
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Jogo Adivinhação
+
+## Introduction
+
+> Este projeto tem como obetivo treinar algoritmos e lógica de programação, com a proposta de criar um jogo que adivinhe um número que o usuário pensou entre 1 e 100 em 9 tentativas. O usuário deve dizer se o número que ele pensou é maior ou menor do que o que o programa informou com os operadores ">" e "<". O programa deve finalizar quando o programa acertar o número e o usuário digitar "=".
